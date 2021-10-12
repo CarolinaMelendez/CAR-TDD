0eaa913f842ef3c0e3a92968e766cf588b4b572f
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-10-12 03:08:56 -0400

    F2. Refactoring

--- src/Functions_car.js
+++ src/Functions_car.js
@@ -1,32 +1,39 @@
+import { parseSync } from "@babel/core";
+
+const isPositive = num => num > 0;
+const isWholeNumber = num => num % 1 == 0;
+const isCorrectNumber = num => isPositive(num) && isWholeNumber(num);
+const isCorrectNumbersPair = vector => isCorrectNumber(vector[0]) &&  isCorrectNumber(vector[1]);
+
 export function calculate_Coordinates(string_Comand){
     let string_Comand_separated = string_Comand.split("/");
-    let comand_dimensions = string_Comand_separated[0];
+    let dimensions = string_Comand_separated[0];
     let initial_position = string_Comand_separated[1];
-
     let default_position = "0,0 N";
-    if (isCorrectFormat_Dimensions(comand_dimensions)){
+    let errorMessage = "Formato incorrecto";
+
+    if (isCorrectFormat_Dimensions(dimensions)){
         if (initial_position){
             if (isCorrectFormat_InitialPosition(initial_position)){
                 return initial_position;
             }else{
-                return "Formato incorrecto";
+                return errorMessage; 
             }
-            
         }else{
             return default_position;
         }
     }else{
-        return "Formato incorrecto";
+        return errorMessage;
     }
+
+
 }
+
+
 
 function isCorrectFormat_Dimensions(string_Comand_dimensions){
     let dimensions = string_Comand_dimensions.split(",").map(Number);
-    const isPositive = num => num > 0;
-    const isWholeNumber = num => num % 1 == 0;
-    const isCorrectNumber = num => isPositive(num) && isWholeNumber(num);
-
-    if (isCorrectNumber(dimensions[0]) && isCorrectNumber(dimensions[1]) ){
+    if (isCorrectNumbersPair(dimensions)){
         return true
     }else{
         return false
@@ -35,15 +42,10 @@ function isCorrectFormat_Dimensions(string_Comand_dimensions){
 
 function isCorrectFormat_InitialPosition(string_Comand_initialPosition){
     let orientation = string_Comand_initialPosition[string_Comand_initialPosition.length -1];
-    let coordinates = string_Comand_initialPosition.replace(orientation,"");
-    let coordinates_pair = coordinates.split(",").map(Number);
-    const isPositive = num => num > 0;
-    const isWholeNumber = num => num % 1 == 0;
-    const isCorrectNumber = num => isPositive(num) && isWholeNumber(num);
-
+    let coordinates_pair = string_Comand_initialPosition.replace(orientation,"").split(",").map(Number);
     const isCorrectOrientation = orientation => orientation == "N" || orientation == "E" || orientation == "S" || orientation == "W";
-
-    if (isCorrectNumber(coordinates_pair[0]) && isCorrectNumber(coordinates_pair[1] && isCorrectOrientation(orientation)) ){
+    
+    if (isCorrectNumbersPair(coordinates_pair) && isCorrectOrientation(orientation)){
         return true
     }else{
         return false
