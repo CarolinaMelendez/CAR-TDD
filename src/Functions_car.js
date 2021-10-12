fcd364d033abff7dc8b48d90d5223838500446b4
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2021-10-12 01:56:36 -0400

    F1. refactoring

--- src/Functions_car.js
+++ src/Functions_car.js
@@ -1,9 +1,20 @@
 export function calculate_Coordinates(string_Comand){
-    let dimensions = string_Comand.split(",").map(Number);
-    if (dimensions[0] > 0 && dimensions[1] > 0 && dimensions[1] % 1 == 0 && dimensions[0] % 1 == 0){
+    if (isCorrectFormat_Dimensions(string_Comand)){
         return "0,0 N"
     }else{
         return "Formato incorrecto"
     }
 }
 
+function isCorrectFormat_Dimensions(string_Comand_dimensions){
+    let dimensions = string_Comand_dimensions.split(",").map(Number);
+    const isPositive = num => num > 0;
+    const isWholeNumber = num => num % 1 == 0;
+    const isCorrectNumber = num => isPositive(num) && isWholeNumber(num);
+
+    if (isCorrectNumber(dimensions[0]) && isCorrectNumber(dimensions[1]) ){
+        return true
+    }else{
+        return false
+    }
+}
